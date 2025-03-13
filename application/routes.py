c86523a668ca3d3d2ae38960ae1d568009d4e23d
Israel Showell <showellisrael0@gmail.com>
2025-03-13 15:31:50 -0400

    Version 1.00

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app, db
-from flask import render_template, flash, redirect, url_for, get_flashed_messages
+from flask import json, render_template, flash, redirect, url_for, get_flashed_messages
 from application.form import UserInputForm
 from application.models import IncomeExpense
 
@@ -36,7 +36,7 @@ def add_expense():
 
 @app.route("/delete/<int:id>")
 def delete(id):
-    
+
     #Query the database to get the entry with the specified id
     entry = IncomeExpense.query.get(id)
 
@@ -49,3 +49,37 @@ def delete(id):
         flash('Transaction not found', 'danger')
 
     return redirect(url_for('index'))
+
+
+
+@app.route("/dashboard")
+def dashboard():
+    income_vs_expense = db.session.query(db.func.sum(IncomeExpense.amount), IncomeExpense.type).group_by(IncomeExpense.type).order_by(IncomeExpense.type).all()
+
+    category_comparison = db.session.query(db.func.sum(IncomeExpense.amount), IncomeExpense.category).group_by(IncomeExpense.category).order_by(IncomeExpense.category).all()
+
+    dates = db.session.query(db.func.sum(IncomeExpense.amount), IncomeExpense.date).group_by(IncomeExpense.date).order_by(IncomeExpense.date).all()
+
+    income_category = []
+    for amounts, _ in category_comparison:
+        income_category.append(amounts)
+
+    income_expense = []
+    for total_amount, _ in income_vs_expense:
+        income_expense.append(total_amount)
+
+    over_time_expenditure = []
+    dates_label = []
+    for amount, date in dates:
+        dates_label.append(date.strftime("%m-%d-%y"))
+        over_time_expenditure.append(amount)
+
+    print(dates_label)
+    print(over_time_expenditure)
+
+    return render_template('dashboard.html', title = 'Dashboard',
+                            income_vs_expense=json.dumps(income_expense),
+                            income_category=json.dumps(income_category),
+                            over_time_expenditure=json.dumps(over_time_expenditure),
+                            dates_label =json.dumps(dates_label)
+                        )
